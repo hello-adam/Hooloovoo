a83b69b6f68e55d24e8d1146935f62a4e5415d5d
Adam Nash <adam.t.nash@gmail.com>
2011-11-01 13:49:22 -0400

    Add some basic properties to AnimationComponent.  This addresses #3

--- src/animationcomponent.cpp
+++ src/animationcomponent.cpp
@@ -4,4 +4,15 @@ AnimationComponent::AnimationComponent(GameObject *parentObject) :
     Component(parentObject)
 {
     this->setObjectName("Animation Component");
+
+    m_timeStep = 60;
+    m_trigger = AnimationComponent::Constant;
+    m_files = QList<QVariant>();
+}
+
+QSet<QString> AnimationComponent::getEditProperties()
+{
+    QSet<QString> properties;
+    properties << "timeStep" << "pixmapFiles" << "animationTrigger";
+    return properties;
 }
